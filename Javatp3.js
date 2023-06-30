c5aedf7635315083b533a8296368bab824fbdd7a
FernandoMV1 <135079773+FernandoMV1@users.noreply.github.com>
2023-06-29 20:54:59 -0600

    Project 3 - HTML, css, js

---
+++ Javatp3.js
@@ -0,0 +1,8 @@
+function foncEntre(){
+    document.getElementById("maDiv").classList.remove("blanc");
+    document.getElementById("maDiv").classList.add("rouge");
+}
+function foncQuitte(){
+    document.getElementById("maDiv").classList.remove("rouge");
+    document.getElementById("maDiv").classList.add("blanc");
+}
